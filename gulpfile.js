c5ffe5f3837ad13728f32bee7fcde81d32cae8f6
Julia Ver <jverchyonova@gmail.com>
2019-07-22 17:01:43 +0300

    00_Initial structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,22 @@
 ;
 
+const   gulp = require('gulp'),
+        sass = require('gulp-sass');
+
+
+
+// pipe style from src to dist:
+gulp.task('styles', function() {
+    return gulp.src('src/scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css/'))
+    // .pipe(browserSync.reload({stream: true}));
+    });
+
+
+
+
+
+/*********** WATCHERS **********/
+
+
