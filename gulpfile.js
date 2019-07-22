0c62d23ff3a0589179aec88eeadf89913583d3ee
Julia Ver <jverchyonova@gmail.com>
2019-07-22 18:09:34 +0300

    00_Initial structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 ;
 
 const   gulp = require('gulp'),
-        sass = require('gulp-sass');
+        sass = require('gulp-sass'),
+        watch = require('gulp-watch')
+        browserSync = require('browser-sync');
 
 
+/*********** BROWSER SYNC **********/
 
-// pipe style from src to dist:
+// gulp.task('browser-sync', function() {
+//     browserSync({
+//         server: {
+//             baseDir: 
+//         }
+//     });
+// })
+
+
+/*********** style from src to dist **********/
+
 gulp.task('styles', function() {
     return gulp.src('src/scss/style.scss')
     .pipe(sass().on('error', sass.logError))
@@ -13,10 +26,12 @@ gulp.task('styles', function() {
     // .pipe(browserSync.reload({stream: true}));
     });
 
-
+/
 
 
 
 /*********** WATCHERS **********/
-
+gulp.task('watch', function() {
+    gulp.watch('src/scss/**/*.scss', gulp.series('styles'))
+})
 
